71874474a746dd58bae4aa8a1d44e02260e5517c
hrhrng <hrhrng@foxmail.com>
2025-11-09 21:28:08 +0800

    deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-  },
+export default defineConfig(({ mode }) => {
+  const base = process.env.VITE_BASE ?? '/';
+
+  return {
+    base, // ✅ 支持 GitHub Pages 子路径
+    plugins: [react()],
+    server: {
+      port: 3001,
+    },
+  };
 });
